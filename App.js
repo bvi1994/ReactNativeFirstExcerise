7802a054cd9c2d1d1c2e49fe7853632caabea269
bvi1994 <bvi1@ucsc.edu>
2017-10-12 14:52:30 -0700

    Completed 3B. I hate CSS

--- App.js
+++ App.js
@@ -1,76 +1,62 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import _ from 'underscore'
+import { TouchableOpacity, StyleSheet, Text, View , ListView} from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+      super();
+      this.state = {
+        numbers: _.range(11)
+      }
+  }
+
+  // press(item){
+  //   this.setState({
+  //     numbers: this.state.numbers.filter((currentItem) => (item !== currentItem))
+  //   });
+  // }
+
+  remove(){
+    this.setState({
+      numbers: this.state.numbers.slice(0, this.state.numbers.length - 1)
+    })
+  }
+
+  add(){
+    this.setState({
+      numbers: this.state.numbers.concat(this.state.numbers.length)
+    })
+  }
+
   render() {
+    var dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => (r1 !== r2)
+    });
     return (
-      // <View style={{flex: 1}}>
-      //   <View style={styles.backgroundColorRed}></View>
-      //   <View style={styles.backgroundColorBlue}></View>
-      // </View>
-      // <View style={styles.centeredText}>
-      //   <Text>I am centered!</Text>
-      // </View>
-      // <View style={styles.centeredText2}>
-      //   <Text style={{
-      //     color: 'blue',
-      //     fontSize: 100
-      //   }}>Blue</Text>
-      //   <Text style={{
-      //     color: 'red',
-      //     fontSize: 50
-      //   }}>Red</Text>
-      // </View>
-      <View style={styles.centeredText3}>
-        {/* <TouchableOpacity> */}
-          {/* <Text style={{
-            fontSize: 100
-          }}>I love Matthew</Text>
-        </TouchableOpacity> */}
-        <TouchableOpacity><Text style={{
-          fontSize: 100
-        }}>I</Text></TouchableOpacity>
-        <TouchableOpacity><Text style = {{
-          fontSize: 100
-        }}>Love</Text></TouchableOpacity>
-        <TouchableOpacity><Text style = {{
-          fontSize: 100
-        }}>You</Text></TouchableOpacity>
+      <View style={{flex: 1,  marginTop: 30}}>
+        <TouchableOpacity onPress={this.add.bind(this)}><Text style={{fontSize: 50, color: '#1e90ff', textAlign: 'center'}}>Add</Text></TouchableOpacity>
+        <TouchableOpacity onPress={this.remove.bind(this)}><Text style={{fontSize: 50, color: 'red', textAlign: 'center'}}>Remove</Text></TouchableOpacity>
+        <ListView
+          // style={{flex: 1, borderColor: 'red', borderWidth: 1}}
+          renderRow={(item) =>
+            (<TouchableOpacity style={{flex: 1}}>
+              <Text style={{fontSize: 30, textAlign: 'center'}}>{item}</Text>
+            </TouchableOpacity>
+            )}
+          dataSource={dataSource.cloneWithRows(this.state.numbers)}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // backgroundColorRed: {
-  //   backgroundColor: 'red',
-  //   flex: 1
-  // },
-  // backgroundColorBlue: {
-  //   backgroundColor: '#1e90ff',
-  //   flex: 2
-  // }
-  // centeredText:{
-  //   flex: 1,
-  //   flexDirection: 'column',
-  //   alignItems: 'center',
-  //   justifyContent: 'center'
-  // }
-  // centeredText2:{
-  //   flex: 1,
-  //   flexDirection: 'column',
-  //   alignItems: 'center',
-  // }
-  centeredText3:{
+
+  centeredText: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'space-between'
+    marginTop: 30
   }
+
 });
